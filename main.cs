c3b8c54e05a71c280958045c10a3391a5f37900a
jp1621 <jimmy_pham1998@yahoo.com>
2020-06-17 03:38:26 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,13 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Enter a number: 12");
+    Console.WriteLine ("Enter another number: 8");
+    Console.WriteLine ("Sum= 20");
+    Console.WriteLine ("Diff= 8");
+    Console.WriteLine ("Product= 96");
+    Console.WriteLine ("Quotient=2");
+    Console.WriteLine ("Remainder= 4");
+  }
+}
